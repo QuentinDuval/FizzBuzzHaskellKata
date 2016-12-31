cacbbc164f40d625f389c94ab42a9d33b0de56f5
Duval Quentin <senmenty@gmail.com>
2016-12-31 17:32:23 +0100

    more readable tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,15 +8,30 @@ import Test.QuickCheck
 
 -- | Example based tests
 
-testCases :: [(Int, String)]
-testCases = [ (0, "0"), (1, "1"), (3, "Fizz"), (5, "Buzz")
-            , (7, "7"), (15, "FizzBuzz"), (100, "Buzz")]
-
 tests :: Test
-tests = TestList $ map createTestCase testCases
-  where
-    createTestCase (input, expected) =
-      TestCase (assertEqual ("FizzBuzz of " ++ show input) expected (fizzBuzz input))
+tests =
+  let multiples_of_3_and_5 = [3 * 5, 2 * 3 * 5, 3 * 3 * 5]
+      multiples_of_3 = [3, 1 * 3, 2 * 3, 3 * 3]
+      multiples_of_5 = [5, 1 * 5, 2 * 5, 3 * 5]
+      other_numbers = [0, 1, 2, 4]
+  in TestList
+    [ fizzBuzz `should_equal` "FizzBuzz" `for_all` multiples_of_3_and_5
+    , fizzBuzz `should_equal` "Fizz" `for_all` multiples_of_3
+    , fizzBuzz `should_equal` "Buzz" `for_all` multiples_of_5
+    , fizzBuzz `should_do_like` show `for_all` other_numbers
+    ]
+
+should_equal :: (Show t, Show a, Eq a) => (t -> a) -> a -> t -> Test
+should_equal underTest expected input =
+  let label = "Expects " ++ show expected ++ " for " ++ show input
+  in TestCase $ assertEqual label expected (underTest input)
+
+should_do_like :: (Show t, Show a, Eq a) => (t -> a) -> (t -> a) -> t -> Test
+should_do_like underTest refFct input
+  = should_equal underTest (refFct input) input
+
+for_all :: (a -> Test) -> [a] -> Test
+for_all fct inputs = TestList (map fct inputs)
 
 
 -- | QuickCheck fun
